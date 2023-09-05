5db708655ed113427c6d6975ff65d6b970d1f112
Arsenic <airajena07@gmail.com>
2023-09-05 13:38:31 +0530

    template

--- demo.cpp
+++ demo.cpp
@@ -1,11 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// int main(){
+//     int n ;
+//     cin>>n;
+//      while(n%10==0){
+//         n/=10;
+//     }
+//     cout<<n<<endl;
+// }
 int main(){
-    int n ;
+    int n;
     cin>>n;
-     while(n%10==0){
-        n/=10;
-    }
     cout<<n<<endl;
 }
---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+
+signed main(){
+    ios :: sync_with_stdio(false); cin.tie(0);
+    
+    int tc;
+    cin>>tc;
+    while(tc--){
+
+    }
+}
